3c0099b8b4ed7165f8849c98b6890bc1b5989c7b
mercimat <11376662+mercimat@users.noreply.github.com>
2020-10-16 22:29:52 +0200

    add run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH="$WORKSPACE"
+
+mkdir -p $GOPATH/src/github.com/mercimat
+ln -s $WORKSPACE $GOPATH/src/github.com/mercimat/instavote
+
+if [ ! -f ./go.mod ]; then
+    go mod init github.com/mercimat/instavote
+fi
+
+go get -d -v go.mongodb.org/mongo-driver/mongo
+go get -d -v github.com/go-redis/redis/v8
+go get -d -v github.com/google/uuid
+
+cd core/
+go test -v -vet=off .
+
